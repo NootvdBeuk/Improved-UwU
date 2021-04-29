c80eb68dde166fe699338dda6848bbeaf6074376
m31b0k <melvin4@live.nl>
2021-04-29 20:16:25 +0200

    add emerald and directory ensurance to selector

--- selector/main.py
+++ selector/main.py
@@ -7,11 +7,22 @@ if os.path.dirname(os.path.realpath(__file__)) != sys.path[0]:
     print("Could not find script path, exiting")
     sys.exit()
 
+def ensure_path(p):
+    if not os.path.exists(p):
+        os.mkdir(p)
+
 mc_path = os.path.join(os.path.dirname(sys.path[0]), 'assets', 'minecraft')
-block_model_path = os.path.join(mc_path, 'models', 'block')
+model_path = os.path.join(mc_path, 'models')
+block_model_path = os.path.join(model_path, 'block')
+item_model_path = os.path.join(model_path, 'item')
 bed_model_path = os.path.join(block_model_path, 'bed')
 bed_texture_path = os.path.join(mc_path, 'textures', 'blocks', 'beds')
 bed_types = sorted([f for f in os.listdir(bed_texture_path) if os.path.isdir(os.path.join(bed_texture_path, f))])
+
+ensure_path(model_path)
+ensure_path(block_model_path)
+ensure_path(item_model_path)
+ensure_path(bed_model_path)
 
 # -- tkinter stuff --
 
@@ -76,12 +87,21 @@ def set_sand_type():
         with open(os.path.join(block_model_path, 'upper_slab_' + c + 'sandstone.json'), 'w') as f:
             f.write('{"parent":"block/upper_slab","textures":{"bottom":"blocks/' + d + c + 'sandstone_bottom","top":"blocks/' + d + c + 'sandstone_top","side":"blocks/' + d + c + 'sandstone_normal"}}')
 
+# emerald
+emerald_types = ['one', 'two']
+def set_emerald_type():
+    print("setting emerald type to " + emerald.tkvarq.get())
+    d = '1' if emerald.tkvarq.get() == emerald_types[1] else ''
+    with open(os.path.join(item_model_path, 'emerald.json'), 'w') as f:
+        f.write('{"parent": "builtin/generated","textures": {"layer0": "items/emerald' + d + '"},"display": {"thirdperson": {"rotation": [ -90, 0, 0 ],"translation": [ 0, 1, -3 ],"scale": [ 0.55, 0.55, 0.55 ]},"firstperson": {"rotation": [ 0, -135, 25 ],"translation": [ 0, 4, 2 ],"scale": [ 1.7, 1.7, 1.7 ]}}}')
+
 # -- initializing classes --
 
 bed = selector(0, 'bed type', 'set bed type', bed_types, set_bed_type)
 wool = selector(1, 'wool type', 'set wool type', wool_types, set_wool_type)
 sand = selector(2, 'sand type', 'set sand type', sand_types, set_sand_type)
+emerald = selector(3, 'emerald type', 'set emerald type', emerald_types, set_emerald_type)
 # quit button
-Button(root, text='quit', command=root.destroy).grid(row=3, columnspan=3)
+Button(root, text='quit', command=root.destroy).grid(row=10, columnspan=3)
 
 root.mainloop()
